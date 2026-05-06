0187ba8212c87cc1092c32a5c72089515f36dea7
Ctrl <dylanzarzar@gmail.com>
2026-05-06 01:31:08 -0700

    Revise fxmanifest for version 1.1.0
    Updated author name and version. Modified shared and client scripts sections, and added server scripts and new dependencies.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,28 @@
-fx_version 'cerluean'
+fx_version 'cerulean'
 game 'gta5'
 
 lua54 'yes'
 
-author 'Croll Studios'
+author 'NoCtrl Studios'
 description 'Qbox Stripper Pole Script'
-version '1.0.0'
+version '1.1.0'
 
-shared_script '@ox_lib/int.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
 
-client_scipts {
-    'config.lua',
+client_scripts {
     'client.lua'
 }
 
-dependecies {
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
     'ox_target',
-    'ox_lib'
+    'ox_lib',
+    'ox_inventory',
+    'qbx_core'
 }
